cd43f76dff1f14005ac062d046e7fb7606492535
Pavel Durmanov <31005942+P-Alban@users.noreply.github.com>
2018-01-09 15:13:28 +0200

    Add files via upload

--- hastebin.py
+++ hastebin.py
@@ -1,10 +1,14 @@
+import json
 import requests
 
 new_paste_url = 'https://hastebin.com/documents'
 
 def paste(paste_content):
-    response = requests.post(new_paste_url, data=paste_content, timeout=1000)
     try:
-        return response.json()['key']
-    except KeyError:
-        return False
+        response = requests.post(new_paste_url, data=paste_content, timeout=1000).json()
+    except json.decoder.JSONDecodeError:
+        return 'Application Error'
+    if 'key' in response:
+        return response
+    else:
+        return response['message']
