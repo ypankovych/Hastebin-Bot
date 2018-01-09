6647e48d157d89e9b18c56d64274b245ed153dd8
Pavel Durmanov <31005942+P-Alban@users.noreply.github.com>
2018-01-09 19:22:12 +0200

    Add files via upload

--- hastebin.py
+++ hastebin.py
@@ -8,7 +8,7 @@ def paste(paste_content):
         response = requests.post(new_paste_url, data=paste_content, timeout=1000).json()
     except json.decoder.JSONDecodeError:
         return 'Application Error'
-    if 'key' in response:
+    if response.get('key'):
         return response
     else:
         return response['message']
--- main.py
+++ main.py
@@ -10,20 +10,34 @@ bot = telebot.TeleBot(os.environ.get('token'))
 def start(message):
     with open('hello_message.txt') as hello_msg:
         bot.send_message(chat_id=message.chat.id, text=hello_msg.read(), parse_mode='Markdown')
+    botan.track(os.environ.get('botan_key'), message.chat.id, message, 'Start.')
 
 @bot.message_handler(content_types=['document'])
 def documents_handler(message):
     if message.document.mime_type.split('/')[0] == 'text':
         file_info = bot.get_file(message.document.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
-        paste_link = paste(paste_content=downloaded_file.decode(chardet.detect(downloaded_file)['encoding']).encode('utf8'))
-        if isinstance(paste_link, dict):
-            bot.reply_to(message=message, text=f'https://hastebin.com/{paste_link["key"]}', parse_mode='HTML')
+        result = validate_file_content(downloaded_file)
+        if result['status']:
+            bot.reply_to(message=message, text=f'https://hastebin.com/{result["result"]}', parse_mode='HTML')
             botan.track(os.environ.get('botan_key'), message.chat.id, message, 'New paste created.')
         else:
-            bot.send_message(chat_id=message.chat.id, text=f'`Error: {paste_link}`', parse_mode='Markdown')
+            bot.send_message(chat_id=message.chat.id, text=f'`Error: {result["message"]}`', parse_mode='Markdown')
     else:
-        bot.send_message(chat_id=message.chat.id, text='`Error: incorrect file type.`', parse_mode='Markdown')
+        bot.send_message(chat_id=message.chat.id, text='`This file type is not supported.`', parse_mode='Markdown')
+
+@bot.message_handler()
+def info(message):
+    bot.send_message(chat_id=message.chat.id, text='*Only files are supported.*', parse_mode='Markdown')
+
+def validate_file_content(file_object):
+    file_encoding = chardet.detect(file_object)['encoding']
+    if not file_encoding:
+        return {'status': 0, 'message': 'incorrect file type.'}
+    paste_result = paste(paste_content=file_object.decode(file_encoding).encode('utf8'))
+    if isinstance(paste_result, dict):
+        return {'status': 1, 'result': paste_result['key']}
+    return {'status': 0, 'message': paste_result}
 
 if __name__ == '__main__':
     bot.polling(none_stop=True, interval=0)
